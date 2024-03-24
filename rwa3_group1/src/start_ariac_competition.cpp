58caa00fb2f82c49020ec3549499aa2a5849fead
Vinay Krishna <vinaykrishnabukka@gmail.com>
2024-03-23 23:44:57 -0400

    Task 3 updated

--- rwa3_group1/src/start_ariac_competition.cpp
+++ rwa3_group1/src/start_ariac_competition.cpp
@@ -28,11 +28,11 @@ void AriacCompetitionStart::competition_state_subscriber_cb(const ariac_msgs::ms
         current_competition_state = "ENDED";
     }
     RCLCPP_INFO(this->get_logger(),"The current state of competition is %s",current_competition_state.c_str());
-    if(state->competition_state == ariac_msgs::msg::CompetitionState::READY){
-        // current_state = 1;
-        // RCLCPP_INFO_STREAM(this->get_logger(),"Current State: ");
-        start_competition();
-    }
+    // if(state->competition_state == ariac_msgs::msg::CompetitionState::READY){
+    //     // current_state = 1;
+    //     // RCLCPP_INFO_STREAM(this->get_logger(),"Current State: ");
+    //     start_competition();
+    // }
     
     current_state.competition_state = state->competition_state;
     //   auto my_state = state->competition_state;
@@ -42,18 +42,41 @@ void AriacCompetitionStart::competition_state_subscriber_cb(const ariac_msgs::ms
 
 }
 
+void AriacCompetitionStart::start_competition(){
+    RCLCPP_INFO(this->get_logger(),"Waiting for competition to start");
+    if (current_state.competition_state ==  ariac_msgs::msg::CompetitionState::STARTED){
+        return;
+    }
+
+    auto current_node = shared_from_this();
+    while (current_state.competition_state != ariac_msgs::msg::CompetitionState::READY){
+        rclcpp::spin_some(current_node);
+        RCLCPP_INFO(this->get_logger(),"Its not ready yet");
+    }
+    RCLCPP_INFO(this->get_logger(),"Competition is ready. Starting..");
+    if (!start_competition_client_->wait_for_service(std::chrono::seconds((int)(2.0)))){
+        RCLCPP_INFO(this->get_logger(),"Service not available");
+    }
+
+    auto request = std::make_shared<std_srvs::srv::Trigger::Request>();
+    auto future = start_competition_client_->async_send_request(request);
+
+
+    if (rclcpp::spin_until_future_complete(current_node, future) == rclcpp::FutureReturnCode::SUCCESS)
+    {
+        RCLCPP_INFO(this->get_logger(),"Started Competition");
+    } else {
+        RCLCPP_INFO(this->get_logger(),"Unable to start Competition");
+    }
+
+}
+
 // void AriacCompetitionStart::start_competition(){
-//     RCLCPP_INFO(this->get_logger(),"Waiting for competition to start");
 //     if (current_state.competition_state ==  ariac_msgs::msg::CompetitionState::STARTED){
 //         return;
 //     }
-//     // std::shared_ptr<rclcpp::Node> node =
-//     //       rclcpp::Node::make_shared("trigger_client");
-//     auto start_node1 = std::make_shared<AriacCompetitionStart>("trial_start");
-//     while (current_state.competition_state != ariac_msgs::msg::CompetitionState::READY){
-//         rclcpp::spin_some(start_node);
-//         RCLCPP_INFO(this->get_logger(),"Its not ready yet");
-//     }
+//     std::shared_ptr<rclcpp::Node> node =
+//           rclcpp::Node::make_shared("trigger_client");
 //     RCLCPP_INFO(this->get_logger(),"Competition is ready. Starting..");
 //     if (!start_competition_client_->wait_for_service(std::chrono::milliseconds((int)(2000.0)))){
 //         RCLCPP_INFO(this->get_logger(),"Service not available");
@@ -64,8 +87,7 @@ void AriacCompetitionStart::competition_state_subscriber_cb(const ariac_msgs::ms
 
 //     // rclcpp::spin_until_future_complete(node,future);
 
-//     if (rclcpp::spin_until_future_complete(start_node1, future) ==
-//         rclcpp::FutureReturnCode::SUCCESS)
+//     if (rclcpp::spin_until_future_complete(node, future) == rclcpp::FutureReturnCode::SUCCESS)
 //     {
 //         RCLCPP_INFO(this->get_logger(),"Started Competition");
 //     } else {
@@ -73,37 +95,11 @@ void AriacCompetitionStart::competition_state_subscriber_cb(const ariac_msgs::ms
 //     }
 
 // }
-
-void AriacCompetitionStart::start_competition(){
-    if (current_state.competition_state ==  ariac_msgs::msg::CompetitionState::STARTED){
-        return;
-    }
-    std::shared_ptr<rclcpp::Node> node =
-          rclcpp::Node::make_shared("trigger_client");
-    RCLCPP_INFO(this->get_logger(),"Competition is ready. Starting..");
-    if (!start_competition_client_->wait_for_service(std::chrono::milliseconds((int)(2000.0)))){
-        RCLCPP_INFO(this->get_logger(),"Service not available");
-    }
-
-    auto request = std::make_shared<std_srvs::srv::Trigger::Request>();
-    auto future = start_competition_client_->async_send_request(request);
-
-    // rclcpp::spin_until_future_complete(node,future);
-
-    if (rclcpp::spin_until_future_complete(node, future) ==
-        rclcpp::FutureReturnCode::SUCCESS)
-    {
-        RCLCPP_INFO(this->get_logger(),"Started Competition");
-    } else {
-        RCLCPP_INFO(this->get_logger(),"Unable to start Competition");
-    }
-
-}
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
   auto start_node = std::make_shared<AriacCompetitionStart>("trial_start");
-  rclcpp::spin(start_node);
-//   start_node -> start_competition();  
+//   rclcpp::spin(start_node);
+  start_node -> start_competition();  
   
   rclcpp::shutdown();
 }
