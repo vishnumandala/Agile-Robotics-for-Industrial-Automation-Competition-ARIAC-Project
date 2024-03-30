24ea5bc25c1727f806b9123bc3ca393a9394b4be
Vinay Krishna <vinaykrishnabukka@gmail.com>
2024-03-30 18:47:33 -0400

    c++ version trials

---
+++ rwa3_group1/script/order_management_update.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.executors import MultiThreadedExecutor
+from rwa3_group1.order_management_interface_update import OrderManagement
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = OrderManagement("order_management_upate")
+    executor = MultiThreadedExecutor()
+    executor.add_node(node)
+
+    try:
+        executor.spin()
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
